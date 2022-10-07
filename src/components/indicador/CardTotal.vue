ce30b5a28604075e8304b031e4a2da1fdacf5899
Roely Chur <gerson.roely@gmail.com>
2022-10-07 11:02:03 -0600

    remove one decimal

--- src/components/indicador/CardTotal.vue
+++ src/components/indicador/CardTotal.vue
@@ -1,6 +1,6 @@
 <template>
     <span>
-        {{ data.value }}%
+        {{ total_modified }}%
     </span>
 </template>
 
@@ -25,6 +25,27 @@ export default {
             return{
                 'font-size': '10vw'
             }
+        },
+        total_modified(){
+
+            let split_number = this.data.value.toString().split('.')
+
+            if (split_number.length > 1) {
+                
+                if (this.data.value.toString().split('.')[1].length > 1) {
+
+                    const integer = this.data.value.toString().split('.')[0]
+                    
+                    const decimals = this.data.value.toString().split('.')[1].slice(0, -1)
+                    
+                    return integer + '.' + decimals
+
+                }
+
+            }
+
+            return this.data.value
+
         }
     }
 };
