11a57b9c5c060132a489eeef1aa8eb8583f86a51
Roely Chur <gerson.roely@gmail.com>
2022-10-11 11:16:42 -0600

    new detail for Principal KPI

--- src/components/indicador/CardBottomDetail.vue
+++ src/components/indicador/CardBottomDetail.vue
@@ -8,16 +8,16 @@
         >
             <v-row>
                 <v-col cols="12 pb-0 pt-0 title-bottom">
-                    <span class="font-weight-medium ">
+                    <span class="font-weight-medium">
                         {{ item.text }}
                     </span>
                 </v-col>
                 <v-col cols="12 pt-0 pb-0 total-bottom mt-2">
-                    <span class="font-weight-black ">
+                    <span class="font-weight-black">
                         {{ item.value }}
                     </span>
                 </v-col>
-                <v-divider v-if="key < (items.length - 1)" vertical></v-divider>
+                <v-divider v-if="key < items.length - 1" vertical></v-divider>
             </v-row>
         </v-col>
 
@@ -32,28 +32,26 @@
                 ></edit-field>
             </template>
         </custom-dialog>
-
     </v-row>
 </template>
 
 <style scoped>
-.click{
+.click {
     cursor: pointer;
 }
-.title-bottom{
-    font-size: 0.70vw;
+.title-bottom {
+    font-size: 0.7vw;
 }
-.total-bottom{
+.total-bottom {
     font-size: 2vw;
 }
 </style>
 
 <script>
-
 import CustomModal from "@/components/dialog/CustomModal";
 import EditField from "@/components/dialog/EditField";
 
-import { mapMutations, mapState } from 'vuex'
+import { mapMutations, mapState } from "vuex";
 
 export default {
     props: {
@@ -63,45 +61,45 @@ export default {
                 return [];
             },
         },
-        indicador: Object
+        indicador: Object,
     },
     components: {
         "custom-dialog": CustomModal,
         "edit-field": EditField,
-    },  
-    data(){
-        return{
+    },
+    data() {
+        return {
             value: 0,
             placeholder: null,
-            data: null
-        }
+            data: null,
+        };
     },
     methods: {
         ...mapMutations({
-            setShow: 'dialog/setShow',
-            setData: 'dialog/setData',
-            setBottomSelected:  'config/setBottomSelected',
-            updateTotal: 'config/updateTotal'
+            setShow: "dialog/setShow",
+            setData: "dialog/setData",
+            setBottomSelected: "config/setBottomSelected",
+            updateTotal: "config/updateTotal",
         }),
-        setDetail(detail){
-            
-            this.setData(detail)
+        setDetail(detail) {
 
-            this.setShow(true)
+            console.log(detail)
+
+            this.setData(detail);
+
+            this.setShow(true);
         },
-        showEdit(payload){
-            
-            this.value = payload.value
-            this.placeholder = payload.text
-            this.data = payload
+        showEdit(payload) {
+            this.value = payload.value;
+            this.placeholder = payload.text;
+            this.data = payload;
 
-            this.setBottomSelected(payload)
+            this.setBottomSelected(payload);
 
-            this.$refs.dialog.activeDialog(true)
+            this.$refs.dialog.activeDialog(true);
         },
-        updateValue(val){
-
-            this.updateTotal(val)
+        updateValue(val) {
+            this.updateTotal(val);
 
             // this.bottom_selected.value = parseInt(val)
 
@@ -110,29 +108,28 @@ export default {
             // // * Obtener de bottom_detail los identificados como up y down
 
             // let up = this.indicador_get.bottom_detail.find(item => item.divide == 'up')
-            
+
             // let down = this.indicador_get.bottom_detail.find(item => item.divide == 'down')
 
             // const result = up.value / down.value
-            
+
             // if (down.value <= 0 || !down.value || up.value <= 0 || !up.value) {
-                
+
             //     this.indicador_get.content.total.value = 0
 
-            //     return 
+            //     return
             // }
 
             // let total = result * 100
 
             // this.indicador_get.content.total.value = total > 100 ? 100 : Number.isInteger(total) ? total : total.toFixed(1)
-            
-        }
+        },
     },
     computed: {
         ...mapState({
-            bottom_selected: state => state.config.bottom_selected,
-            indicador_get: state => state.config.indicador
-        })
-    }
+            bottom_selected: (state) => state.config.bottom_selected,
+            indicador_get: (state) => state.config.indicador,
+        }),
+    },
 };
 </script>
--- src/components/tables/TableProcesos.vue
+++ src/components/tables/TableProcesos.vue
@@ -1,18 +1,62 @@
 <template>
     <v-card-text>
-        <v-expansion-panels flat>
-            <v-expansion-panel v-for="(item, key) in data_component" :key="key">
-                <v-expansion-panel-header>
-                    {{ item.descripcion }}
-                </v-expansion-panel-header>
-                <v-expansion-panel-content class="pl-0 ml-0">
-                    <custom-component
-                        :data_component="item.bottom_detail"
-                        :name="item.component"
-                    ></custom-component>
-                </v-expansion-panel-content>
-            </v-expansion-panel>
-        </v-expansion-panels>
+        <v-tabs class="mb-4 ml-4" v-model="tab">
+            <v-tab>Resumen</v-tab>
+            <v-tab>Detalle</v-tab>
+        </v-tabs>
+        <v-tabs-items v-model="tab">
+            <v-tab-item>
+                <v-row>
+                    <v-col cols="7">
+                        <v-list>
+                            <v-list-item v-for="(element, key) in dialog_data.tooltip" :key="key">
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ element.title }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                                <v-list-item-action>
+                                    <v-chip label>
+                                        {{ element.value }}
+                                    </v-chip>
+                                </v-list-item-action>
+                            </v-list-item>
+                        </v-list>
+                    </v-col>
+                    <v-col align="center">
+                        <v-divider vertical></v-divider>
+                    </v-col>
+                    <v-col cols="4">
+                        <v-card class="text-center pt-4" outlined>
+                            <span class="text-h1 text-center">
+                                {{ dialog_data.value }}
+                            </span>
+                            <v-card-subtitle>
+                                {{ dialog_data.text }}
+                            </v-card-subtitle>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-tab-item>
+            <v-tab-item>
+                <v-expansion-panels flat>
+                    <v-expansion-panel
+                        v-for="(item, key) in data_component"
+                        :key="key"
+                    >
+                        <v-expansion-panel-header>
+                            {{ item.descripcion }}
+                        </v-expansion-panel-header>
+                        <v-expansion-panel-content class="pl-0 ml-0">
+                            <custom-component
+                                :data_component="item.bottom_detail"
+                                :name="item.component"
+                            ></custom-component>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-tab-item>
+        </v-tabs-items>
     </v-card-text>
 </template>
 
@@ -23,7 +67,10 @@
 </style>
 
 <script>
+
 import CustomCumponent from "@/components/custom/CustomComponent.vue";
+
+import { mapState } from "vuex";
 
 export default {
     props: {
@@ -32,5 +79,15 @@ export default {
     components: {
         "custom-component": CustomCumponent,
     },
+    data() {
+        return {
+            tab: 0,
+        };
+    },
+    computed: {
+        ...mapState({
+            dialog_data: state => state.dialog.data
+        })
+    }
 };
 </script>
